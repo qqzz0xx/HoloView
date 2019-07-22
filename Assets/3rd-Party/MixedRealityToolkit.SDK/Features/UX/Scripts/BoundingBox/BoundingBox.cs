67e2ab8a06ba2a106bd64bd64957bd50e55ad87c
sqz <632574418@qq.com>
2019-07-22 18:36:10 +0800

    Assets\3rd-Party\MixedRealityToolkit.SDK\Features\UX\Scripts\BoundingBox\BoundingBox.cs

--- Assets/3rd-Party/MixedRealityToolkit.SDK/Features/UX/Scripts/BoundingBox/BoundingBox.cs
+++ Assets/3rd-Party/MixedRealityToolkit.SDK/Features/UX/Scripts/BoundingBox/BoundingBox.cs
@@ -109,6 +109,20 @@ namespace Microsoft.MixedReality.Toolkit.UI
         [FormerlySerializedAs("BoxColliderToUse")]
         private BoxCollider boundsOverride = null;
 
+        public BoxCollider BoundsOverride
+        {
+            get { return boundsOverride; }
+            set
+            {
+                if (boundsOverride != value)
+                {
+                    boundsOverride = value;
+                    CreateRig();
+                }
+            }
+        }
+
+
         [Header("Behavior")]
         [SerializeField]
         private BoundingBoxActivationType activation = BoundingBoxActivationType.ActivateManually;
