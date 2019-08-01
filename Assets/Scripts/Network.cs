6297418276be08d3b921fccbe2a7f5b4ed1ebf86
sqz <632574418@qq.com>
2019-08-01 18:48:15 +0800

    Network

---
+++ Assets/Scripts/Network.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+public class Network : MonoBehaviour
+{
+    public static Network Inst;
+
+    IPAddress address;
+    int port;
+
+    private Socket socket;
+    private bool connected;
+
+    private ManualResetEvent timeoutEvent = new ManualResetEvent(false);
+    private int timeoutMSec = 8000;    //connect timeout count in millisecond
+
+    private SocketAsyncEventArgs sendArgs;
+    private SocketAsyncEventArgs recvArgs;
+
+    private Transports transports;
+
+    public static IPAddress GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip;
+            }
+        }
+        throw new Exception("No network adapters with an IPv4 address in the system!");
+    }
+
+    private void Awake()
+    {
+        Inst = this;
+        Init();
+    }
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    public void Init()
+    {
+        address = GetLocalIPAddress();
+        port = 5555;
+        transports = new Transports();
+        transports.onDispatch = Receive;
+
+        sendArgs = new SocketAsyncEventArgs();
+        recvArgs = new SocketAsyncEventArgs();
+
+    }
+    public void Connect()
+    {
+        var tmpSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        var ipend = new IPEndPoint(address, port);
+
+        timeoutEvent.Reset();
+
+        try
+        {
+            tmpSocket.BeginConnect(ipend, p =>
+            {
+                connected = true;
+                socket = tmpSocket;
+
+            }
+            , tmpSocket);
+        }
+        finally
+        {
+            timeoutEvent.Set();
+        }
+
+        if (timeoutEvent.WaitOne(timeoutMSec, false))
+        {
+            Debug.LogError("Connect: timeout" );
+        }
+
+    }
+
+    void Update()
+    {
+        if (connected)
+        {
+            if (socket.ReceiveAsync(recvArgs))
+            {
+                transports.ProBytes(recvArgs.Buffer, 0, recvArgs.Buffer.Length);
+            }
+        }
+    }
+
+    void Receive(int msgid, byte[] msgdata)
+    {
+        Debug.Log("Recv: " + msgid);
+    }
+
+    void Send(int msgid, byte[] msgdata)
+    {
+        var buffer = transports.PackBytes(msgid, msgdata);
+        sendArgs.SetBuffer(buffer, 0, buffer.Length);
+        socket.SendAsync(sendArgs);
+    }
+
+    void SendHeartBeat()
+    {
+        Send(-1, null);
+    }
+
+}
---
+++ Assets/Scripts/Transports.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using UnityEngine;
+
+enum TransportState
+{
+    ePaserHead,
+    ePaserBody,
+}
+
+public class Transports 
+{
+    private const int s_MsgHeadLen = 4;
+
+    private TransportState m_TransportState = TransportState.ePaserHead;
+    //proto
+    private byte[] m_MsgHead = new byte[s_MsgHeadLen];
+    private byte[] m_MsgBody;
+    private int m_iHadCopyHead;
+    private int m_iHadCopyBody;
+
+
+    private int m_iMsgID;
+    private int m_iSeqID;
+    private int m_iMsgBodyLen;
+
+
+    public Action<int, byte[]> onDispatch = null;
+
+
+    public byte[] PackBytes(int msgid, byte[] body)
+    {
+        byte[] buffer = new byte[body.Length + s_MsgHeadLen];
+        buffer[0] = Convert.ToByte(msgid & 0xFF);
+        buffer[1] = Convert.ToByte(msgid >> 8 & 0xFF);
+        buffer[2] = Convert.ToByte(body.Length & 0xFF);
+        buffer[3] = Convert.ToByte(body.Length >> 8 & 0xFF);
+
+        WriteBytes(body, 0, buffer, 4, body.Length);
+
+        return buffer;
+    }
+    public void ProBytes(byte[] buffer, int offset, int length)
+    {
+        if (buffer == null || length <= 0 || offset >= length) return;
+        int iDataLen = length - offset;
+        while (iDataLen > 0)
+        {
+            if (m_TransportState == TransportState.ePaserHead)
+            {
+                int iCopyHead = s_MsgHeadLen - m_iHadCopyHead;
+                if (iDataLen >= iCopyHead)
+                {
+                    WriteBytes(buffer, offset, m_MsgHead, m_iHadCopyHead, iCopyHead);
+                    offset += iCopyHead;
+                    iDataLen -= iCopyHead;
+                    m_iHadCopyHead = 0;
+
+                    m_iMsgID = m_MsgHead[0] + (m_MsgHead[1] << 8);
+                    m_iMsgBodyLen = m_MsgHead[2] + (m_MsgHead[3] << 8);
+
+                    if (m_iMsgBodyLen > 0)
+                    {
+                        m_iHadCopyBody = 0;
+                        m_MsgBody = new byte[m_iMsgBodyLen];
+                        m_TransportState = TransportState.ePaserBody;
+                    }
+                    else
+                    {
+                        if (onDispatch != null)
+                        {
+                            onDispatch(m_iMsgID, m_MsgBody);
+                        }
+                        ResetPaserState();
+                    }
+                }
+                else
+                {
+                    WriteBytes(buffer, offset, m_MsgHead, m_iHadCopyHead, iDataLen);
+                    m_iHadCopyHead += iDataLen;
+                    return;
+                }
+            }
+            else if (m_TransportState == TransportState.ePaserBody)
+            {
+                int iCopyBody = m_iMsgBodyLen - m_iHadCopyBody;
+                if (iDataLen >= iCopyBody)
+                {
+                    WriteBytes(buffer, offset, m_MsgBody, m_iHadCopyBody, iCopyBody);
+                    offset += iCopyBody;
+                    iDataLen -= iCopyBody;
+                    if (onDispatch != null)
+                    {
+                        onDispatch(m_iMsgID, m_MsgBody);
+                    }
+                    ResetPaserState();
+                }
+                else
+                {
+                    WriteBytes(buffer, offset, m_MsgBody, m_iHadCopyBody, iDataLen);
+                    m_iHadCopyBody += iDataLen;
+                    return;
+                }
+
+            }
+        }
+    }
+
+    private void ResetPaserState()
+    {
+        m_TransportState = TransportState.ePaserHead;
+        m_iHadCopyBody = 0;
+        m_iHadCopyHead = 0;
+    }
+
+    private void WriteBytes(byte[] src, int srcOffset, byte[] dst, int dstOffset, int length)
+    {
+        Buffer.BlockCopy(src, srcOffset, dst, dstOffset, length);
+    }
+}
